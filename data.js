aaad7bbe89e9020aed3da6b3ab48811fdbe5e541
Hsaadeh <hsaadeh622@gmail.com>
2021-04-12 01:11:22 -0500

    setting up

---
+++ data.js
@@ -0,0 +1,13 @@
+let projects = [
+    {"name": "City Owned Land Inventory Filter",
+    "tech": "HTML,Javascript,CSS,RWD,Google Maps Api",
+    "url_to_page": "",
+    "url_to_repo": "",
+    "date": "Spring 2021"},
+
+    {"name": "2D Side Scroller",
+    "tech": "Sprites,Animation,HTML,Javascript,CSS,RWD",
+    "url_to_page": "",
+    "url_to_repo": "",
+    "date": "Spring 2021"}
+];
